c3b18cf4dbbe5a38a873fc99280c599a6b1e59f6
Brandon <brandonlevis84@gmail.com>
2022-10-28 00:42:36 -0400

    Add project files.

---
+++ LibraryService/Controllers/BooksController.cs
@@ -0,0 +1,37 @@
+﻿using LibraryService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LibraryServices.Data.Models;
+using LibraryServices.Data.Interfaces;
+
+namespace LibraryService.Controllers
+{
+    public class BooksController : ApiController
+    {
+        private IBookRepository books;
+        public BooksController(IBookRepository _book) {
+            this.books = _book;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return books.GetAllBooks();
+        }
+
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            var book = books.GetBook(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+    }
+}
